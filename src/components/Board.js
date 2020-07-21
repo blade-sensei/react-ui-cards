dbd36566ce20d28f64051696b855bc97f1f32a68
Juan COYLA <jcoyla123@gmail.com>
2020-07-21 15:16:57 +0200

    chore: add grid layout

---
+++ src/components/Board.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './Board.css';
+
+function Board () {
+
+  // data init
+
+  // function internal
+
+  return (
+    <div className='container'>
+      <div className='cards-jobday'></div>
+      <div className='cards-jobapplication'></div>
+      <div className='cards-topsalaries'></div>
+      <div className='cards-profiles'></div>
+
+    </div>
+  )
+}
+
+export default Board
