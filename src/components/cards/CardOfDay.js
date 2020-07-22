c71b6111c3a7d6202c44216e8c66ec8aa49778eb
Juan COYLA <jcoyla123@gmail.com>
2020-07-22 19:06:53 +0200

    chore: job day cards components layout

--- src/components/cards/CardOfDay.js
+++ src/components/cards/CardOfDay.js
@@ -4,7 +4,20 @@ function CardOfDay() {
 
   return (
     <div className='card'>
-      card of the day
+      <div className='card-header'>
+        <div className='card-avatar'> 
+          <img src='' alt=''/>
+          <span> title spotify</span>
+        </div>
+        <div className='card-rating'>
+          <span> 4.4 </span>
+        </div>
+      </div>
+      <div className='card-body'>
+        <div className='prof-title'> title prof </div>
+        <div className='salary'> 1222</div>
+        <div className='location'>location</div>
+      </div>
     </div>
   )
 }
